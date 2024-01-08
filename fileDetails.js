fa201ca99538edc4a991ae49082d0de6d5c126ba
TusharAnpat <tusharanpat7777@gmail.com>
2024-01-08 12:07:01 +0530

    server created

---
+++ fileDetails.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const FileDetailsScehma = new mongoose.Schema(
+  {
+   file:String
+  },
+  {
+    collection: "FileDetails",
+  }
+);
+
+mongoose.model("FileDetails", FileDetailsScehma);
